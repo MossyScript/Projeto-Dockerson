0e63a5bfa11f49cb498ef8627052185061a64c42
DonutHorror <bloodydonutz@gmail.com>
2025-07-15 21:47:08 -0300

    Configuração do email

--- ISP/Mail/init.sh
+++ ISP/Mail/init.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
+# Verifica se os Maildirs já existem no volume, se não, copia do backup interno
+if [ ! -d "/home/dockerson/Maildir" ];      then
+    cp -r /maildata/dockerson /home/
+fi
 
+if [ ! -d "/home/kuberneterson/Maildir" ];  then
+    cp -r /maildata/kuberneterson /home/
+fi
+
+if [ ! -d "/home/dnsilson/Maildir" ];       then
+    cp -r /maildata/dnsilson /home/
+fi
+
+chown -R dockerson:dockerson            /home/dockerson
+chown -R kuberneterson:kuberneterson    /home/kuberneterson
+chown -R dnsilson:dnsilson              /home/dnsilson
+
+# Iniciar serviços
 service syslog-ng start
 service dovecot start
 service postfix start
 
-tail -F /var/log/mail.log
+# Manter container rodando
+tail -f /dev/null
